f19f7becec647191f0ecbc477efbc65c07493148
Alex Babkin <zephyr-eng+ababkin@gust.com>
2014-09-11 18:27:28 -0400

    some progress

--- src/Main.hs
+++ src/Main.hs
@@ -3,33 +3,32 @@
 
 module Main where
 
-import           Data.Text
 import           Haste.App                 (addChild, liftIO, mkConfig, newElem,
                                             newTextElem, onServer, remote,
                                             runApp, runClient, withElem)
 
-import           Control.Monad.Trans.Class (lift)
-import           Pipes                     (Producer (..), runEffect, yield,
-                                            (>->), (>~))
-import           Pipes                     (await, next)
-import qualified Pipes.Attoparsec          as PA
-import           Pipes.Parse               (runStateT)
-import qualified Pipes.Prelude             as P
-import qualified Pipes.Text                as Text
-import qualified Pipes.Text.IO             as Text
+{- import           Data.Text -}
+{- import           Control.Monad.Trans.Class (lift) -}
+{- import           Pipes                     (Producer (..), runEffect, yield, -}
+                                            {- (>->)) -}
+{- import           Pipes                     (await) -}
+{- import qualified Pipes.Attoparsec          as PA -}
+{- import           Pipes.Parse               (runStateT) -}
+{- import qualified Pipes.Prelude             as P -}
+{- import qualified Pipes.Text                as Text -}
+{- import qualified Pipes.Text.IO             as Text -}
 
-import           Parsers
-import           Types
+{- import           Parsers -}
+{- import           Types -}
 
 
-requests :: (Monad m) => Producer Text m () -> Producer Request m String
-requests s = do
-  (r,s') <- lift (runStateT (PA.parse requestParser) s)
-  case r of
-    Nothing -> return ""
-    Just (Left err) -> return ""
-    Just (Right request) -> yield request >> (return "")
-    {- Just (Right request) -> yield request >> requests s' -}
+{- requests :: (Monad m) => Producer Text m () -> Producer Request m String -}
+{- requests s = do -}
+  {- (r,s') <- lift (runStateT (PA.parse requestParser) s) -}
+  {- case r of -}
+    {- Nothing -> return "" -}
+    {- Just (Left err) -> return "" -}
+    {- Just (Right request) -> yield request >> (return "") -}
 
 {- main :: IO () -}
 {- main = do -}
@@ -42,9 +41,9 @@ main = do
   runApp (mkConfig "ws://localhost:24601" 24601) $ do
 
     getRequest <- remote $ do
-      {- liftIO getLine -}
-      liftIO $ runEffect $ do
-        requests Text.stdin >-> P.map show >-> await
+      liftIO getLine
+      {- liftIO $ runEffect $ do -}
+        {- requests Text.stdin >-> P.map show >-> await -}
 
     runClient $ withElem "requests" (renderRequest getRequest)
       where

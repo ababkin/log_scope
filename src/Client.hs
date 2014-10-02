904d26158556e0053253e455bd034a719821c4cc
Gust <gust@fermium.local>
2014-10-02 15:25:25 -0400

    add new bootstrap theme

---
+++ src/Client.hs
@@ -0,0 +1,108 @@
+module Client (render) where
+
+import           Haste.App                 (addChild, liftIO, mkConfig, newElem,
+                                            newTextElem, onServer, remote,
+                                            runApp, runClient, setClass, setAttr,
+                                            withElem, MonadIO, alert)
+import           Control.Applicative       ((<$>))
+import           Control.Monad             (join)
+import           Haste                     (Event (..), onEvent)
+import           Haste.DOM                 (Elem, setClass, toggleClass)
+import           Haste.JSON
+import           Haste.Prim
+import           Haste.Serialize
+
+import Types
+
+render = renderRequest 0
+
+renderRequest n getRequest requestsContainer = do
+  eitherRequest <- decodeJSON . toJSStr <$> onServer getRequest
+  case join $ fromJSON <$> eitherRequest of
+    Right req -> do
+      addRequest req requestsContainer
+      renderRequest (n + 1) getRequest requestsContainer
+    Left err -> do
+      addError err requestsContainer
+      renderRequest (n + 1) getRequest requestsContainer
+
+  where
+    addRequest :: MonadIO m => Request -> Elem -> m ()
+    addRequest req requestsContainer = do
+      request <- newElem "a"
+
+      setAttr request "href" "#"
+      setClass request "list-group-item" True
+
+      input <- newElem "input"
+      setAttr input "id" $ requestId n
+      setAttr input "name" "accordion-1"
+      setAttr input "type" "checkbox"
+      input `addChild` request
+
+      label <- newElem "label"
+      setAttr label "for" $ requestId n
+      
+      requestShort <- newElem "span"
+      appendTextElWithClasses "span" (verb req) ["label", verbCssClass req] requestShort
+      appendTextElWithClasses "span"  (path req) ["path", "pull-right", "text-muted", "small"] requestShort
+      {- appendTextElWithClasses "span"  (controller req) ["controller"] requestShort -}
+      {- appendTextElWithClasses "span"  (action req) ["action"] requestShort -}
+
+      setClass requestShort "request" True
+      requestShort `addChild` label
+      label `addChild` request
+
+      article <- newElem "article"
+      setClass article "ac-small" True
+      p <- newElem "p"
+      textArticle <- newTextElem "more stuff"
+      textArticle `addChild` p
+      p `addChild` article
+      article `addChild` request
+
+
+
+      request `addChild` requestsContainer
+
+      {- onEvent request OnClick $ \_ _ -> toggleRequestExpand request -}
+      return ()
+
+
+      where
+        requestId :: Int -> String
+        requestId n = "ac-" ++ (show n)
+
+        verbCssClass request = case verb req of
+          "GET"     -> "get"
+          "POST"    -> "post"
+          "PUT"     -> "put"
+          "DELETE"  -> "delete"
+          _         -> "unexpected_verb"
+
+        toggleRequestExpand request = do
+          toggleClass request "expanded"
+
+        appendTextElWithClasses :: MonadIO m => String -> String -> [String] -> Elem -> m ()
+        appendTextElWithClasses tag text cssClasses parent = do
+          el <- newElem tag
+
+          textEl <- newTextElem text
+          textEl `addChild` el
+
+          mapM_ (\c -> setClass el c True) cssClasses
+          el `addChild` parent
+
+
+
+    addError :: MonadIO m => String -> Elem -> m ()
+    addError errorText requestsContainer = do
+      err <- newElem "div"
+
+      errText <- newTextElem errorText
+      errText `addChild` err
+
+      setClass err "alert" True
+      setClass err "alert-danger" True
+      err `addChild` requestsContainer
+
--- src/LogScope.hs
+++ src/LogScope.hs
@@ -34,6 +34,7 @@ import           Haste.Prim
 import           Haste.Serialize
 
 import           Types
+import           Client (render)
 
 
 #ifdef __HASTE__
@@ -67,67 +68,5 @@ main = do
 
     getRequest <- remote requests
 
-    runClient $ withElem "requests" (renderRequest getRequest)
-      where
-        renderRequest getRequest requestsContainer = do
-          eitherRequest <- decodeJSON . toJSStr <$> onServer getRequest
-          case join $ fromJSON <$> eitherRequest of
-            Right req -> do
-              addRequest req requestsContainer
-              renderRequest getRequest requestsContainer
-            Left err -> do
-              addError err requestsContainer
-              renderRequest getRequest requestsContainer
-
-          where
-            addRequest :: MonadIO m => Request -> Elem -> m ()
-            addRequest req requestsContainer = do
-              request <- newElem "div"
-
-              appendTextElWithClasses "span" (verb req) ["label", verbCssClass req] request
-              appendTextElWithClasses "div"  (path req) ["path"] request
-              appendTextElWithClasses "div"  (controller req) ["controller"] request
-              appendTextElWithClasses "div"  (action req) ["action"] request
-
-              setClass request "request" True
-              request `addChild` requestsContainer
-
-              onEvent request OnClick $ \_ _ -> toggleRequestExpand request
-              return ()
-
-
-              where
-                verbCssClass request = case verb req of
-                  "GET"     -> "get"
-                  "POST"    -> "post"
-                  "PUT"     -> "put"
-                  "DELETE"  -> "delete"
-                  _         -> "unexpected_verb"
-
-                toggleRequestExpand request = do
-                  toggleClass request "expanded"
-
-                appendTextElWithClasses :: MonadIO m => String -> String -> [String] -> Elem -> m ()
-                appendTextElWithClasses tag text cssClasses parent = do
-                  el <- newElem tag
-
-                  textEl <- newTextElem text
-                  textEl `addChild` el
-
-                  mapM_ (\c -> setClass el c True) cssClasses
-                  el `addChild` parent
-
-
-
-            addError :: MonadIO m => String -> Elem -> m ()
-            addError errorText requestsContainer = do
-              err <- newElem "div"
-
-              errText <- newTextElem errorText
-              errText `addChild` err
-
-              setClass err "alert" True
-              setClass err "alert-danger" True
-              err `addChild` requestsContainer
-
+    runClient $ withElem "requests" (render getRequest)
 

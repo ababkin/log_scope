854e2682b62f61ab628996d9e3e2f9307648fa5e
Alex Babkin <ababkin@gmail.com>
2014-10-23 23:26:57 -0400

    show last expanded request

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -10,3 +10,8 @@ function scrollDown(){
 $(document).on("shown.bs.collapse", ".panel", function (e) {
   $("body").animate({ scrollTop: $(this).offset().top - 100 }, 500);
 });
+
+$(document).on("show.bs.collapse", ".panel", function (e) {
+  $(".request").removeClass("current");
+  $(this).find(".request").addClass("current");
+});
